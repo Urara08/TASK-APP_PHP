d4730be3c45bd71875c0d65324a8012183a48030
Urara <90606372+Urara08@users.noreply.github.com>
2026-02-19 16:00:41 +0900

    Add files via upload
    CSSを追加しました。

--- index.php
+++ index.php
@@ -3,35 +3,38 @@
   <head>
     <meta charset="UTF-8">
     <title>タスク管理アプリ</title>
+    <link rel="stylesheet" href="style.css">
   </head>
-
   <body>
-    <ul>
-      <?php
-      $content = file_get_contents('data.json');
-      $Task_line = json_decode($content, true);
-      ?>
-
-      <h2>現在のタスク状況</h2>
-      <ul>
-        <?php //タスクがなければ
-        if (empty($Task_line)){
-          echo "<sp>" ."未完了タスクはありません"."</sp>" ;}
-
-          else{
-            foreach($Task_line as $key =>$input_task){
-              echo "<li>" .($key + 1) . '：' . htmlspecialchars($input_task, ENT_QUOTES, 'UTF-8') . "</li>";
-              }}?>
-              </ul>
-              <br>
-              <form method="post" action="input.php">
-                <input type="text" name="input_task" id="label-input_task" placeholder="追加するタスク">
-                <input type="submit" value="登録する">
-                <br>
-              </form>
-              <form method="post" action="delete.php">
-                <input type="text" name="delete_task" id="label-delete_task" placeholder="削除するタスク番号">
-                <input type="submit" value="削除する">
-              </form>
+    <main  class="container">
+      <div class="dq-window-2">
+        <div class="dq-window-3">
+          <ul>
+            <?php
+            $content = file_get_contents('data.json');
+            $Task_line = json_decode($content, true);
+            ?>
+            <p class="title">👾現在のタスク状況👾</p>
+            <ul>
+              <?php //タスクがなければ
+              if (empty($Task_line)){
+                echo "<sp class=>" ."＊未完了タスクはありません"."</sp>" ;}
+                else{
+                  foreach($Task_line as $key =>$input_task){
+                    echo "<li>" .($key + 1) . '：' . htmlspecialchars($input_task, ENT_QUOTES, 'UTF-8') . "</li>";
+                    }}?>
+                    </ul>
+                    <form method="post" action="input.php">
+                      <input type="text" name="input_task" id="label-input_task" placeholder="追加するタスク">
+                      <input type="submit" value="登録" class="button">
+                      <br>
+                    </form>
+                    <form method="post" action="delete.php">
+                      <input type="text" name="delete_task" id="label-delete_task" placeholder="削除するタスク番号">
+                      <input type="submit" value="削除" class="button">
+                    </form>
+                  </div>
+                </div>
+              </main>
             </body>
             </html>
