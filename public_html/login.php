27cc54c8741945ddaf53b52977047e9ca81add96
Urara <90606372+Urara08@users.noreply.github.com>
2026-02-21 01:28:33 +0900

    Add files via upload
    CSSを微調整

--- public_html/login.php
+++ public_html/login.php
@@ -6,11 +6,11 @@
     <link rel="stylesheet" href="style.css">
   </head>
   <body>
-    <main  class="container">
+    <main  class="container pd-top">
       <div class="dq-window-2">
             <p class="top">IDとパスワードを入力してください</p>
                     <form method="post" action="login_check.php">
-                      <input type="text" name="id" id="label-input_task" placeholder="ID" class="bottom"><br>
+                      <input type="text" name="id" id="label-input_task" placeholder="ID" class="top bottom"><br>
                       <input type="password" name="password" id="label-delete_task" placeholder="パスワード" class="bottom"><br>
                       <input type="submit" value="ログイン" class="button button_login">
                     </form>
