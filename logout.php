aceb41dd5659d4ea160151563c02e6354dbd689a
Urara <90606372+Urara08@users.noreply.github.com>
2026-02-25 22:04:13 +0900

    Delete logout.php

--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();              // セッション開始
-$_SESSION = [];               // セッション変数を空にする
-session_destroy();            // セッションを破棄する
-header("Location: login.php"); // ログイン画面へ戻す
-exit;
-?>
